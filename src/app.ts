6d166c55aa0239b1df2b2dd36f93c3a800149d48
goni715 <halesgoni715149123@gmail.com>
2024-10-13 19:08:32 +0600

    server-running

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Application, Express, Request, Response } from "express";
 
-const app : Application = express();
+const app: Application = express();
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("This is Todo Server with Typescript");
+});
 
-app.get('/', (req, res)=> {
-    res.send('This is Todo Server with Typescript')
-})
+console.log("Goni Halesv Gopalganj");
 
 export default app;
